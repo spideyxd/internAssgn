ccf02c49148cedec91041aa3f3d6337b43252cf9
Shivam Negi <shivam2612002@gmail.com>
2023-01-23 06:14:46 +0530

    final

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import ResponsiveAppBar from "./Navbar";
 export default function Layout({ children }) {
   return (
     <>
+    
       <ResponsiveAppBar/>
       <main>{children}</main>   
     </>
